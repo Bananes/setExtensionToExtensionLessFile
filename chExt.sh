119b86704a5e81c0562b92c87f2b5a88f63e46f7
imakumo_mac127 <imakumo_mac127@MacBook-Pro-de-imakumo-mac127.local>
2017-08-31 17:08:06 +0200

    Adding the shell script

---
+++ chExt.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+for f in /etc/snc-mid-dev/agent/resumes/*; do
+  pot="$(file $f -b --mime-type)"
+  echo "${pot}"
+    case ${pot} in
+      "application/pdf")
+      mv "$f" "$f.pdf"
+        ;;
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+      mv "$f" "$f.docx"
+        ;;
+      "application/msword")
+      mv "$f" "$f.doc"
+        ;;
+      "text/rtf")
+      mv "$f" "$f.rtf"
+        ;;
+      "application/vnd.oasis.opendocument.text")
+      mv "$f" "$f.odt"
+        ;;
+      *)
+      echo "NIET"
+        ;;
+    esac
+done
