7edc9173f7d2d847d9fcdb5a10d6a434f48bd85f
Thibaudeau Vincent <vincent.thbd@gmail.com>
2017-09-01 15:55:05 +0200

    Adding color and verification
    Verification if the file already has an extension

--- chExt.sh
+++ chExt.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 for f in /your/path/and/files/*; do
-  pot="$(file $f -b --mime-type)"
+  pot="$(file $f -b --mime-type)";
+  RED='\033[0;31m'
+  GREEN='\033[0;32m'
+  NC='\033[0m'
+  echo $f
   echo "${pot}"
+  if [[ $f == *.pdf ]] || [[ $f == *.doc ]] || [[ $f == *.rtf ]] || [[ $f == *.odt ]] || [[ $f == *.docx ]]; then
+    echo "Has an extension"
+  else
+    echo "${GREEN}Does not have an extension, we will add one${NC}"
     case ${pot} in
       "application/pdf")
       mv "$f" "$f.pdf"
@@ -19,7 +27,12 @@ for f in /your/path/and/files/*; do
       mv "$f" "$f.odt"
         ;;
       *)
-      echo "NIET"
+      echo "${RED}Error - we will not add extension to this file${NC}"
         ;;
     esac
+  fi
 done
+
+echo "\n*** Please verify your file ***"
+ls
+
